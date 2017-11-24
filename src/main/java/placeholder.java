b84fb701a47e21cf866c6fe801971936884ead77
messi122 <33869339+messi122@users.noreply.github.com>
2017-11-24 16:11:21 +0800

    增加了set和get方法

--- src/main/java/placeholder.java
+++ src/main/java/placeholder.java
@@ -2,9 +2,47 @@
 public class placeholder {
  
 	public String fenzhi_1_1;
-	
 	public String fenzhi;
 	public String fenzhi2;
-
 	public String masterfenzhi1;
+	public String name;
+	public String gender;
+	public String getFenzhi_1_1() {
+		return fenzhi_1_1;
+	}
+	public void setFenzhi_1_1(String fenzhi_1_1) {
+		this.fenzhi_1_1 = fenzhi_1_1;
+	}
+	public String getFenzhi() {
+		return fenzhi;
+	}
+	public void setFenzhi(String fenzhi) {
+		this.fenzhi = fenzhi;
+	}
+	public String getFenzhi2() {
+		return fenzhi2;
+	}
+	public void setFenzhi2(String fenzhi2) {
+		this.fenzhi2 = fenzhi2;
+	}
+	public String getMasterfenzhi1() {
+		return masterfenzhi1;
+	}
+	public void setMasterfenzhi1(String masterfenzhi1) {
+		this.masterfenzhi1 = masterfenzhi1;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	
 }
