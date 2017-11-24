907b2c565df6a2b25dcf7c022e2fbeb05d93a75e
Jack-husx <jack_husx@163.com>
2017-11-24 17:01:42 +0800

    tttttttttt

--- src/main/java/placeholder.java
+++ src/main/java/placeholder.java
@@ -4,7 +4,6 @@ public class placeholder {
 	public String fenzhi_1_1;
 	public String friskj___;
 	public String fenzhi;
-	public String fenzhi2;
 	public String fenzhi2;	
 
 	public String masterfenzhi1;
