6fb7a8d21064b066259bce2d47685ddf9f7ef41d
jeffrey <315340106@qq.com>
2017-11-25 11:48:46 +0800

    增加了index界面

--- src/main/java/cn/tarena/book/controller/HelloController.java
+++ src/main/java/cn/tarena/book/controller/HelloController.java
@@ -1,11 +1,11 @@
-package cn.tarena.jieshu.controller;
+package cn.tarena.book.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import cn.tarena.jieshu.service.UserService;
+import cn.tarena.book.service.UserService;
 
 @Controller
 public class HelloController {
