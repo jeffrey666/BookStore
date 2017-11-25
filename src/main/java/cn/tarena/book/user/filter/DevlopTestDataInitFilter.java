0f3acc30b8d943e86180f63d635b07bda2647ae9
Arti-young-wild-free <33945264+Arti-young-wild-free@users.noreply.github.com>
2017-11-25 15:05:19 +0800

    解决bug

--- src/main/java/cn/tarena/book/user/filter/DevlopTestDataInitFilter.java
+++ src/main/java/cn/tarena/book/user/filter/DevlopTestDataInitFilter.java
@@ -8,10 +8,20 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 
 import cn.tarena.book.pojo.User;
 import cn.tarena.book.pojo.UserInfo;
 
+
+/**
+ * 使用注解标注过滤器
+ * @WebFilter将一个实现了javax.servlet.Filter接口的类定义为过滤器
+ * 属性filterName声明过滤器的名称,可选
+ * 属性urlPatterns指定要过滤 的URL模式,也可使用属性value来声明.(指定要过滤的URL模式是必选属性)
+ * 
+ */
+@WebFilter(filterName="DevlopTestDataInitFilter",urlPatterns="/*")
 public class DevlopTestDataInitFilter implements Filter {
 
 	@Override
