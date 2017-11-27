c50f3e8b69c458f2ece70da3af9d8b8dc33b9a2e
chenba5730 <33949622+chenba5730@users.noreply.github.com>
2017-11-27 16:42:39 +0800

    cb

---
+++ src/main/webapp/staticfile/js/checkbox.js
@@ -0,0 +1,7 @@
+/* 全选*/
+function checkAll(who, obj){
+	var curCheckBox = document.getElementsByName(who);
+	for(i = 0; i < curCheckBox.length; i++){
+		curCheckBox.item(i).checked = obj.checked;
+	}
+}
