8620784dbc578b2b29c92fd388fc054b5dfbaa97
Arti-young-wild-free <33945264+Arti-young-wild-free@users.noreply.github.com>
2017-11-27 19:59:35 +0800

    shiro权限设置完善

--- src/main/java/cn/tarena/book/service/UserServiceImpl.java
+++ src/main/java/cn/tarena/book/service/UserServiceImpl.java
@@ -62,5 +62,13 @@ public class UserServiceImpl implements UserService {
 	public List<Book> findMyBookListByUserIdReturn(String userId) {
 		return userMapper.findMyBookListByUserIdReturn(userId);
 	}
+
+	@Override
+	public String findRoleByUserId(String userId) {
+		
+		User user = userMapper.findUserById(userId);
+		
+		return user.getRole();
+	}
 	
 }
