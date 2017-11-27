0aae1ba480d51ca4bc514bcd79d91fcefeaf6ecf
messi122 <33869339+messi122@users.noreply.github.com>
2017-11-27 14:56:51 +0800

    提取配置文件类

--- src/main/java/cn/tarena/book/StartBookApplication.java
+++ src/main/java/cn/tarena/book/StartBookApplication.java
@@ -18,17 +18,6 @@ public class StartBookApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StartBookApplication.class, args);
 	}	
-	//配置mybatis的分页插件pageHelper
-	@Bean
-	public PageHelper pageHelper(){
-		  PageHelper pageHelper = new PageHelper();
-		  Properties properties = new Properties();
-		  properties.setProperty("offsetAsPageNum","true");
-		  properties.setProperty("rowBoundsWithCount","true");
-		  properties.setProperty("reasonable","true");
-		  properties.setProperty("dialect","mysql");    //配置mysql数据库的方言
-		  pageHelper.setProperties(properties);
-		  return pageHelper;
-     }
+	
 }
 
