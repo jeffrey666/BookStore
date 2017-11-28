754c4e9e3cd0963537989b376df42530484b2a4c
Arti-young-wild-free <33945264+Arti-young-wild-free@users.noreply.github.com>
2017-11-28 16:21:01 +0800

    开发的时候 salt 改成固定的字符串，方便开发

--- src/main/java/cn/tarena/book/user/utils/MD5Tool.java
+++ src/main/java/cn/tarena/book/user/utils/MD5Tool.java
@@ -5,11 +5,12 @@ import org.apache.shiro.crypto.hash.Md5Hash;
 public class MD5Tool {
 
 	public static final int M_HASHITERATIONS = 2;
+	public static final String TEST_USERNAME = "TEST_USERNAME";
 
 	public static String getMD5(String username,
 			String password) {
 
-		Md5Hash md5Hash = new Md5Hash(password, username,
+		Md5Hash md5Hash = new Md5Hash(password, TEST_USERNAME,
 				M_HASHITERATIONS);
 
 		return md5Hash.toString();
